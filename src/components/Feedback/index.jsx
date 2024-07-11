45a8e399859ded61922600fdf832be749faf3894
syariful umam <syarifulumam7@gmail.com>
2024-07-11 14:38:46 +0700

    add : feedback

---
+++ src/components/Feedback/index.jsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from '@mui/material';
+
+const Feedback = () => (
+  <Box
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1rem',
+      textAlign: 'center',
+      height: '100%',
+      justifyContent: 'center',
+    }}
+  >
+    <img src="./src/static/images/icon-thank-you.svg" alt="checklist" width={100} style={{ alignSelf: 'center' }} />
+    <Typography variant="h5" fontWeight="bold" color="hsl(213, 89%, 18%)">
+      Thank you!
+    </Typography>
+    <Typography color="gray">
+      Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please
+      feel free to email us at support@loremgaming.com.
+    </Typography>
+  </Box>
+);
+
+export default Feedback;
